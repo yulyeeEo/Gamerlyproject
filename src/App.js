435def0bbbd03d95c60f3ae3ce983a3dcdb1bb83
하율 <126836992+yulyeeEo@users.noreply.github.com>
2024-12-06 07:52:08 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import Question from './Question';
+import { questions } from './Questions';
+import Result from './Result';
+import RandomImage from './Randomimage';
+
+function MainTitle() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/main');
+  };
+
+  return (
+    <div onClick={handleClick} className="main-title">
+      <img src="assets/images/Title.png" alt="Main Title" />
+    </div>
+  );
+}
+
+function MainScreen() {
+  const navigate = useNavigate();
+
+  const startGame = () => {
+    navigate('/question');
+  };
+
+  return (
+    <div>
+      <img src="assets/images/main_image.png" alt="Main Image" />
+      <button className="centerButton" onClick={startGame}>
+        <img src="assets/images/Button.png" alt="Button" />
+      </button>
+    </div>
+  );
+}
+
+function Home() {
+  return <MainTitle />;
+}
+
+function App() {
+  const [answers, setAnswers] = useState([]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/main" element={<MainScreen />} />
+        <Route path="/question" element={<Question questions={questions} answers={answers} setAnswers={setAnswers} />} />
+        <Route path="/result" element={<Result answers={answers} />} />
+        <Route path="/random-image" element={<RandomImage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Questions.js
@@ -0,0 +1,108 @@
+export const questions = [
+    {
+      question: "새로운 신작게임이 나왔다! 뭐부터 할까?",
+      choices: [
+        { text: "일단 레벨업부터! 장비도 맞춰야지 바쁘다 바빠~", genre: "rpg" },
+        { text: "낚시도할 수 있네~ 집도 지을 수 있잖아? 당장 해봐야지", genre: "Simulation" }
+      ]
+    },
+    {
+      question: "썸녀(썸남)가 영화를 보자고 한다. 근데.. 공포영화잖아?",
+      choices: [
+        { text: "스릴있겠다!! 당장 보러간다", genre: "Horror" },
+        { text: "그건 너무 무서울 것 같아..다른거보자 ㅠㅠ", genre: "Simulation" }
+      ]
+    },
+    {
+      question: "적의 기지에 몰래 들어갔다. 앞에 적 병사가 보인다.",
+      choices: [
+        { text: "재빨리 총을 꺼내서 사격을 준비한다.", genre: "fps" },
+        { text: "조용히 다가가 대화로 해결할 방법을 찾는다.", genre: "rpg" }
+      ]
+    },
+    {
+      question: "친구랑 게임을 하고있는데 갑자기 살려달라고 한다. 어떻게할까?",
+      choices: [
+        { text: "인생은 혼자야! 친구 버리고간다.", genre: "Co-op" },
+        { text: "조금만 기다려! 바로 구하러간다.", genre: "Simulation" }
+      ]
+    },
+    {
+      question: "길을 걷고있는데 갈림길이 나왔다! 어디로 갈까?",
+      choices: [
+        { text: "위험해보이지만 재미있어 보이는 길로 간다.", genre: "Adventure" },
+        { text: "따분해보이지만 안전해보이는 길로간다.", genre: "Simulation" }
+      ]
+    },
+    {
+      question: "방금뭐지? 괴생명체가 지나간 것 같은데..",
+      choices: [
+        { text: "흥미로우니 조용히 따라가본다.", genre: "Adventure" },
+        { text: "위협적이니 일단 총으로 쏘고본다.", genre: "fps" }
+      ]
+    },
+    {
+      question: "무인도에 친구와 단둘이 떨어졌다. 이럴때 나는?",
+      choices: [
+        { text: "친구와 함께 헤쳐나간다.", genre: "Co-op" },
+        { text: "식량이 부족할 수 있으니 친구를 버리고 혼자간다.", genre: "Adventure" }
+      ]
+    },
+    {
+      question: "즐겨하는 게임에 신대륙이 업데이트됐다!",
+      choices: [
+        { text: "레벨부터 올려야지! 사냥부터 하러간다.", genre: "rpg" },
+        { text: "신기한곳 투성이잖아? 맵부터 둘러본다.", genre: "Adventure" }
+      ]
+    },
+    {
+      question: "게임을 즐길때 나를 더 흥미롭게 하는것은?",
+      choices: [
+        { text: "1인 또는 소수의 팀원으로 경쟁하는 것", genre: "fps" },
+        { text: "다수의 팀원과 협력하여 목표를 달성하는 것", genre: "Co-op" }
+      ]
+    },
+    {
+      question: "내가 게임을 선택할때 가장 중요하게 생각하는 것은?",
+      choices: [
+        { text: "캐릭터의 성장과 흥미로운 이야기", genre: "rpg" },
+        { text: "긴장감과 스릴이 있는 색다른 경험", genre: "Horror" }
+      ]
+    },
+    {
+      question: "한 곳으로 떨어져야만 한다면 나는?",
+      choices: [
+        { text: "신비로운 숲속을 탐험하고 새로운 풍경을 탐험할 수 있는 버섯숲", genre: "Adventure" },
+        { text: "긴장감 넘치는 전장과 형형색색의 조명이 어우러지는 도시", genre: "fps" }
+      ]
+    },
+    {
+      question: "웹툰세계에 빙의해버렸다! 어느세계로 온거지?",
+      choices: [
+        { text: "마법이 가득하고 용사나 모험가가 탐험하는 환상적인 판타지세계", genre: "rpg" },
+        { text: "생존전투를 벌이며 여러 적들과 대치하는 서바이벌 도시", genre: "fps" }
+      ]
+    },
+    {
+      question: "기이한 현상이 일어나는 페가를 조사하던 중 길을 잃어 팀원과 길이 갈렸다.",
+      choices: [
+        { text: "혼자서 탐험을 계속하며 단서를 찾는다.", genre: "Horror" },
+        { text: "팀원과 재합류할 방법을 찾는다.", genre: "Co-op" }
+      ]
+    },
+    {
+      question: "어두운 방에서 이상한 소리가 들렸다.",
+      choices: [
+        { text: "혹시모르니 친구는 앞에서 기다리게하고 혼자 들어갔다온다.", genre: "Horror" },
+        { text: "무서우니 친구와 함께 들어가본다.", genre: "Co-op" }
+      ]
+    },
+    {
+      question: "정체불명의 발자국을 발견했다.",
+      choices: [
+        { text: "발자국을 따라가서 무슨 일이 일어났는지 알아본다.", genre: "Horror" },
+        { text: "발자국의 크기와 형태를 분석하고 어떤 생물인지 추론한다.", genre: "Simulation" }
+      ]
+    }
+  ];
+  
